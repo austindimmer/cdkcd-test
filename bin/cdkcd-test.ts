8119aa4e5b549edb7fe453b5344b6795f5f96f95
Elad Ben-Israel <benisrae@amazon.com>
2019-07-16 16:08:37 +0300

    change pipeline env

--- bin/cdkcd-test.ts
+++ bin/cdkcd-test.ts
@@ -6,7 +6,7 @@ import { PipelineStack } from '../lib/pipeline';
 
 const app = new cdk.App();
 
-new PipelineStack(app, 'CdkcdPipeline', { env: { region: 'us-west-2', account: '585695036304' }});
+new PipelineStack(app, 'CdkcdPipeline', { env: { region: 'us-east-1', account: '585695036304' }});
 
 new CdkcdTestStack(app, 'CdkcdTestStack1', { env: { region: 'us-west-2', account: '585695036304' }});
 new CdkcdTestStack(app, 'CdkcdTestStack2', { env: { region: 'us-east-1', account: '585695036304' }});
