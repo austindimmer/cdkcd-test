a67d8a3168e994d8b56cd7853e00da2f6e15cefd
Elad Ben-Israel <benisrae@amazon.com>
2019-07-16 17:17:13 +0300

    add a fargate service because why not

--- bin/cdkcd-test.ts
+++ bin/cdkcd-test.ts
@@ -3,16 +3,21 @@ import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
 import { CdkcdTestStack } from '../lib/cdkcd-test-stack';
 import { PipelineStack } from '../lib/pipeline';
+import { WebApp } from '../lib/web-app';
 
 const app = new cdk.App();
 
+const PIPELINE_ACCOUNT = '585695036304';
+const ANOTHER_ACCOUNT = '138197434366';
+
 new PipelineStack(app, 'CdkcdPipeline', {
-  env: { region: 'us-east-1', account: '585695036304' },
+  env: { region: 'us-east-1', account: PIPELINE_ACCOUNT },
   stacks: [
-    new CdkcdTestStack(app, 'CdkcdTestStack1', { env: { region: 'us-west-2', account: '585695036304' }}),
-    new CdkcdTestStack(app, 'CdkcdTestStack2', { env: { region: 'us-east-1', account: '585695036304' }}),
-    new CdkcdTestStack(app, 'CdkcdTestStack3', { env: { region: 'us-east-1', account: '138197434366' }}),
-    new CdkcdTestStack(app, 'CdkcdTestStack4', { env: { region: 'eu-west-1', account: '138197434366' }}),
+    new WebApp(app, 'CdkcdWebApp', { env: { region: 'eu-west-2', account: ANOTHER_ACCOUNT } }),
+    new CdkcdTestStack(app, 'CdkcdTestStack1', { env: { region: 'us-west-2', account: PIPELINE_ACCOUNT } }),
+    new CdkcdTestStack(app, 'CdkcdTestStack2', { env: { region: 'us-east-1', account: PIPELINE_ACCOUNT } }),
+    new CdkcdTestStack(app, 'CdkcdTestStack3', { env: { region: 'us-east-1', account: ANOTHER_ACCOUNT } }),
+    new CdkcdTestStack(app, 'CdkcdTestStack4', { env: { region: 'eu-west-1', account: ANOTHER_ACCOUNT } }),
   ]
 });
 
---
+++ lib/web-app.ts
@@ -0,0 +1,18 @@
+import { Stack, Construct, StackProps } from '@aws-cdk/core';
+import ecsp = require('@aws-cdk/aws-ecs-patterns');
+import ecs = require('@aws-cdk/aws-ecs');
+import ec2 = require('@aws-cdk/aws-ec2');
+
+export class WebApp extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, 'vpc');
+    const cluster = new ecs.Cluster(this, 'cluster', { vpc: vpc });
+
+    new ecsp.LoadBalancedFargateService(this, 'service', {
+      image: ecs.ContainerImage.fromAsset(__dirname + '../image'),
+      cluster: cluster
+    });
+  }
+}
